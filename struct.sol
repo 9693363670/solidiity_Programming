73952e510b142e8771633359a16e51bc0e0deac9
Ritik-gupta <162976918+9693363670@users.noreply.github.com>
2026-03-18 22:15:35 +0530

    Add Solidity contract with student structure

---
+++ struct.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier-GPL:3.0
+pragma solidity >0.5.0<0.9.0;
+
+contract structure
+{
+    student public s1;
+
+    struct student
+    {
+        uint roll;
+        string name;
+    }
+
+    constructor (uint _roll, string memory _name)
+    {
+        s1.roll=_roll;
+        s1.name=_name;
+    }
+
+    function change(uint _roll , string memory _name) public 
+    {
+        student memory new_student=student({
+            roll:_roll,
+            name:_name
+        });
+
+        
+
+        s1=new_student;
+    }
+}
